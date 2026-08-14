7315c809a039e157b2f515a914e7fbaf01aea865
Zettaspy <emerold2113@gmail.com>
2026-08-14 16:54:31 -0400

    Git repo initalization and structure suggestion

---
+++ quickGuide/1_operatorsAndDatatypes.py
@@ -0,0 +1,35 @@
+"""
+Operators and Data Types:
+    
+    This file explains the different operators, datatypes, and results of those operators and how thye apply to data types.
+
+    To begin lets get you used to some comments and simple operators
+"""
+
+#This is a single line comment   : signle line comments stop being a comment at the next new line of a file.
+
+"""
+ this is a doc string and is considered a multilined comment
+ these two forms of comments will direct you and give you information throughout this guide.
+"""
+
+
+#The assignment operator
+
+x = 1
+
+"""
+    As you can see the assignment OPERATOR is the equal sign, the letter and the number area variable and integer respectively. These are known as OPERANDS.
+"""
+
+
+#in python variables do not need an explicit type declaration like int for a value of 1 (like so int x = 1)
+
+#The Addition, Multiplication, Subtraction, and Division Operators
+
+x + 1   #(this results in 2)
+x - 1   #(this results in 0)
+x * 5   #(this results in 5)
+x / 0   #(this results in a NaN or more commonly known as NOT A NUMBER)
+
+
---
+++ quickGuide/README.txt
@@ -0,0 +1,10 @@
+Hey!  Just to give some helpful tips to remember and cultivate new understanding for Python. I am creatign a full guide on python in the directory to take q quick glance at if needed. Also please read the files in numerical order. Thanks!
+
+
+Tips:
+	1. Always try to do some examples yourself or thoughts before looking at the answer (not always possible)
+	2. If you want, take notes! I will be more than happy to answer questions or if I am not available google can
+	3. Have fun and pace yourself, no point in doing something you dont enjoy as it means you wont remember anything.
+
+
+	GLHF  :D
