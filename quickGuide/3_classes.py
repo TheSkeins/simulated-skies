57ee322cd0fc44a9dfd6c201db6ee82527bfab54
Zettaspy <emerold2113@gmail.com>
2026-08-17 16:27:57 -0400

    file restruct

---
+++ quickGuide/3_classes.py
@@ -0,0 +1,18 @@
+"""
+Classes
+
+This gets into object oriented design.
+an object is segment of code that is copyied or instantiated in memory.
+Its memory address is its Unique ID
+
+Technically in python you make classes (which are the abstract form of how an object will behave) Rather than  put it in memory.
+Once it is in memory it becomes an object. For this jsut assume class = object.
+
+Lets see how to create them.
+"""
+
+class Name:                        #This is the declaration for an object, this lets python know that it is 
+    def __init__(self):
+        pass
+
+    
