3d24c54b304b5c526e8bdf87605d22b8697090d2
Zettaspy <emerold2113@gmail.com>
2026-09-14 13:21:52 -0400

    More restructuring and file creation

--- src/main/World/Terrain/TerrainCell.py
+++ src/main/World/Terrain/TerrainCell.py
@@ -1,6 +1,10 @@
 class TerrainCell:
-    def __init__(self):
-        pass
+    def __init__(self, TerrainType:str, Height:int, Temperature:float, Humidity:float, Albedo:int):
+        self.TerrainType = TerrainType
+        self.Height      = Height
+        self.Temperature = Temperature
+        self.Humidity    = Humidity
+        self.Albedo      = Albedo
 
     def __str__(self):
-        pass
+        return f"{self.TerrainType} | {self.Height} | {self.Temperature} | {self.Humidity} | {self.Albedo}"
